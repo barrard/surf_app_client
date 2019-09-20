a9b65e39ab6a1e3fe5f6942079dc3d2c631f1cc0
David Barrar <barrard@gmail.com>
2019-09-20 03:36:18 -1000

    diff layer

--- src/components/Maps/createMap.js
+++ src/components/Maps/createMap.js
@@ -33,7 +33,7 @@ export default function createMap({ lat, lng }) {
       // accessToken:
     }
   );
-  MapBox.addTo(map);
+  Esri_WorldImagery.addTo(map);
 
   // tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
